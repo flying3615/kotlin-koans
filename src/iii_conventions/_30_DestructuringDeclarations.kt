f568f902bafd49c967359d966aae3002d5b11e62
liuyufei <gabriel.liu3615@gmail.com>
2017-06-23 00:19:37 +1200

    conventions

--- src/iii_conventions/_30_DestructuringDeclarations.kt
+++ src/iii_conventions/_30_DestructuringDeclarations.kt
@@ -14,9 +14,13 @@ fun todoTask30(): Nothing = TODO(
 class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 fun isLeapDay(date: MyDate): Boolean {
-    todoTask30()
-//    val (year, month, dayOfMonth) = date
-//
-//    // 29 February of a leap year
-//    return year % 4 == 0 && month == 2 && dayOfMonth == 29
+
+    val (year, month, dayOfMonth) = date
+
+    // 29 February of a leap year
+    return year % 4 == 0 && month == 2 && dayOfMonth == 29
 }
+
+private operator fun  MyDate.component1() = this.year
+private operator fun  MyDate.component2() = this.month
+private operator fun  MyDate.component3() = this.dayOfMonth
